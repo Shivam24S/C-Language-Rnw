d504893a6f787e73da190a2d5da8c6e58109a46a
Shivam24S <shivamshrivastav8055@gmail.com>
2025-10-02 17:30:20 +0530

    intro and escape sequence character code added

---
+++ ch-1 introduction/2.escape.c
@@ -0,0 +1,24 @@
+
+
+// escape sequence character
+
+
+#include <stdio.h>
+
+int main(){
+
+
+    // /n
+
+
+    printf("hello\nworld");
+
+    printf("\ngood\tmorning\n");
+
+    printf("display\\slash");
+
+    printf("\ndisplay\"coma");
+
+    return 0;
+
+}
