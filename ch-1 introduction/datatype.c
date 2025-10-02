2a8062743d2a30841e9989799c2bbf902b085f72
Shivam24S <shivamshrivastav8055@gmail.com>
2025-10-02 17:57:09 +0530

    dataTypes and  format specifier code added

---
+++ ch-1 introduction/datatype.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+
+int main(){
+
+    int num = 24;
+
+    printf("num:%d",num);
+
+
+    char* str ="john";
+
+    printf("\nstring:%s",str);
+
+
+    float pi = 3.14;
+
+    printf("\ndecimal value:%f",pi);
+
+
+
+    return 0;
+}
