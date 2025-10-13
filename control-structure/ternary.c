f2467d0ccc34e8ffa26c3cd8c7adfa250c64fd75
Shivam24S <shivamshrivastav8055@gmail.com>
2025-10-13 18:08:08 +0530

    flowChart and ternary and switch case  code added

--- control-structure/ternary.c
+++ control-structure/ternary.c
@@ -1,24 +1,30 @@
 
 
-
 #include <stdio.h>
 
 
 int main(){
 
 
-    int a=10,b=20;
 
- 
+    int a=5;
+
+    int b=10;
+
+
     // if(a>b){
     //     printf("a is greater than b");
-    // }else {
+    // }else{
     //     printf("b is greater than a");
     // }
 
+    // syntax
 
-    (a>b)?printf("a is greater than b"):printf("b is greater than a");
+    // () ? :
 
+
+    (a<b)?printf("a is greater than b") :printf("b is greater than a");
 
     return 0;
+
 }
