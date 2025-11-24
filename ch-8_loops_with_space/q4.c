dd4df10e6837812fb8fcc6f01dbde5d56caf8120
Shivam24S <shivamshrivastav8055@gmail.com>
2025-11-24 17:59:44 +0530

    loops with space qns added

--- ch-8_loops_with_space/q4.c
+++ ch-8_loops_with_space/q4.c
@@ -6,17 +6,19 @@ int main()
     
     int num=5;
     
+    
     for(int i=5;i>=1;i--){
-        
-        for(int j=1;j<i;j++){
+        for(int j=1;j<=num-i;j++){
             printf(" ");
         }
-        for(int k=i;k<=num;k++){
-            printf("%d",k);
+        for(int k=i;k>=1;k--){
+            printf("%d",k%2);
         }
         printf("\n");
-        
     }
+  
+    
+    
 
     return 0;
 }
